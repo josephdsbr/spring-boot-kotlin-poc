4ff9afd2a26e75883a0726276da3e85b18ab4e00
josephdsbr <josephdsbr@gmail.com>
2021-04-22 10:16:47 -0300

    Ajustando configurações do banco.

--- src/main/kotlin/com/flexpag/kotlinpoc/models/User.kt
+++ src/main/kotlin/com/flexpag/kotlinpoc/models/User.kt
@@ -4,5 +4,5 @@ import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
 
-@Entity
+@Entity(name = "users")
 class User(@Id @GeneratedValue val id: Long = 0L, val name: String = "", val password: String = "")
